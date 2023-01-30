1bd9509f8ef9ab9128a25ab8baf82be79361402b
MadhugiriMahesh <mahesh.madhugiri@gmail.com>
2023-01-30 21:37:45 +0530

    Commit1

--- src/basic/concepts/HelloWorld.java
+++ src/basic/concepts/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello Word!");
-		System.out.println("Hello Word! Mahesh");
+		System.out.println("Hello Word! Mahesh - Remote");
 
 	}
 }
